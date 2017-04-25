2ec92bc113cd213b82318e18fbac23687b4927ba
darrellboey <15056233@myrp.edu.sg>
2017-04-25 12:40:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15056233/demolistview/Food.java
@@ -0,0 +1,21 @@
+package com.example.a15056233.demolistview;
+
+/**
+ * Created by 15056233 on 25/4/2017.
+ */
+
+public class Food {
+    private String name;
+    private boolean star;
+    public Food(String name, boolean star) {
+        this.name = name;
+        this.star = star;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isStar() {
+        return star;
+    }
+}
+
